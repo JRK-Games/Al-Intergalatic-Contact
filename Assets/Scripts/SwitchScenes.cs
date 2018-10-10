684f9235c99d0003111be81d70b233bcff7a79cb
Gecko <jefferysmith6020@gmail.com>
2018-10-10 03:32:57 -0400

    Intro Scene Finished
    Scene for Intro movie is finished and scripted.

--- Assets/Scripts/SwitchScenes.cs
+++ Assets/Scripts/SwitchScenes.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class SwitchScenes : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // use this for initialization
+    void Start() {
+
+    }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown (KeyCode.Space)) {
-            SceneManager.LoadScene ("MainMenu");
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            SceneManager.LoadScene("Main Menu");
         }
     }
 }
